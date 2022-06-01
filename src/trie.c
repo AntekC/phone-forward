9a6014fbf12988a025876ab472a65c009f325e33
Antek <46371916+AntekC@users.noreply.github.com>
2022-06-01 13:31:12 +0200

    Update trie.c

--- src/trie.c
+++ src/trie.c
@@ -144,21 +144,15 @@ void deleteNumberFromReverse(Trie *reverse, char const *number_reverse, char con
         reverse = reverse->child[index];
     }
 
-    printf("1\n");
-    bool test = areNumbersIndentical(phnumGet(reverse->numbers,0),number_to_delete);
-    printf("11\n");
-    if(reverse->numbers == NULL){
-        printf("NULL reverse\n");
+    if(reverse != NULL){
+        if(areNumbersIndentical(phnumGet(reverse->numbers,0),number_to_delete)){
+            printf("2\n");
+            phnumDeleteFirstNumber(&reverse->numbers);
+        } else {
+            printf("3\n");
+            phnumDeleteNumber(reverse->numbers,number_to_delete);
+        }
     }
-
-    if(areNumbersIndentical(phnumGet(reverse->numbers,0),number_to_delete)){
-        printf("2\n");
-        phnumDeleteFirstNumber(&reverse->numbers);
-    } else {
-        printf("3\n");
-        phnumDeleteNumber(reverse->numbers,number_to_delete);
-    }
-
 }
 
 void deleteAllNumberStartingWith(Trie *reverse, char const *number_reverse, char const *prefix){
